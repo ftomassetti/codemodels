c478eadd7df516b256b3246f23a0b45f0f57bd67
ftomassetti <FTomassetti@FTOMASSETTI-PC>
2013-08-30 16:57:12 +0200

    Possibility to iter on genrated models on the fly

--- lib/lightmodels/model_building.rb
+++ lib/lightmodels/model_building.rb
@@ -8,6 +8,20 @@ class << self
 	attr_accessor :verbose
 end
 
+def self.handle_models_in_dir(src,src_extension,error_handler=nil,model_handler,&model_creator)
+	puts "== #{src} -> #{dest} ==" if LightModels::ModelBuilding.verbose
+	Dir["#{src}/*"].each do |fd|		
+		if File.directory? fd
+			basename = File.basename(fd)
+			handle_models_in_dir("#{src}/#{basename}",src_extension,error_handler,model_handler,&model_creator)
+		else
+			if File.extname(fd)==".#{src_extension}"				
+				handle_model_per_file(fd,error_handler,model_handler,&model_creator)
+			end
+		end
+	end
+end
+
 def self.generate_models_in_dir(src,dest,src_extension,dest_extension,max_nesting=500,error_handler=nil,&model_creator)
 	puts "== #{src} -> #{dest} ==" if LightModels::ModelBuilding.verbose
 	Dir["#{src}/*"].each do |fd|		
@@ -22,6 +36,24 @@ def self.generate_models_in_dir(src,dest,src_extension,dest_extension,max_nestin
 				generate_model_per_file(fd,translated_name,max_nesting,error_handler,&model_creator)
 			end
 		end
+	end
+end
+
+def self.handle_model_per_file(src,error_handler=nil,model_handler,&models_generator)
+	puts "<Model from #{src}>"
+	
+	if error_handler
+		begin
+			m = models_generator.call(src)
+			model_handler.handle(src,m)
+		rescue Exception => e
+			error_handler.call(src,e)
+		rescue
+			error_handler.call(src,nil)
+		end
+	else
+		m = models_generator.call(src)
+		model_handler.call(src,m)
 	end
 end
 
