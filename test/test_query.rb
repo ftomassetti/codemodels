c7bd6f6f78b9191d139a09aea08ee17e42d3b383
ftomassetti <FTomassetti@FTOMASSETTI-PC>
2013-09-10 22:38:47 +0200

    Some tests added

--- test/test_query.rb
+++ test/test_query.rb
@@ -5,10 +5,18 @@ include LightModels
 
 class TestStats < Test::Unit::TestCase
 
+	EcoreLiterals = JavaUtilities.get_proxy_class('org.eclipse.emf.ecore.EcorePackage$Literals')
+
 	Pack = EMF.create_epackage('my_pack','my_pack_uri')
 
 	Person = EMF.create_eclass Pack
 	Person.eStructuralFeatures.add EMF.create_eattribute_str('name')
+
+	Address = EMF.create_eclass Pack
+	Address.eStructuralFeatures.add EMF.create_eattribute_str('street')
+	Address.eStructuralFeatures.add EMF.create_eattribute('number',EcoreLiterals::ELONG)
+
+	Person.eStructuralFeatures.add EMF.create_ereference(Address, 'home_address', [:containment])
 
 	def test_collect_values_empty
 		p = EMF.create_eobject(Person)
@@ -24,6 +32,21 @@ class TestStats < Test::Unit::TestCase
 
 		assert_equal 1,Query.collect_values(m['root']).count
 		assert Query.collect_values(m['root']).include? 'Federico'
+	end
+
+	def test_collect_values_in_children
+		home = EMF.create_eobject(Address)
+		home.set_attr_value 'street','via Tripoli'
+		home.set_attr_value 'number',27
+		federico = EMF.create_eobject(Person)
+		federico.set_attr_value 'name','Federico'
+		federico.set_ref_value 'home_address', home
+
+		map = LightModels::Query.collect_values_with_count(Serialization.eobject_to_model(federico)['root'])
+		assert_equal 3,map.count
+		assert_equal 1,map['via Tripoli']
+		assert_equal 1,map['Federico']
+		assert_equal 1,map[27]
 	end
 
 end
