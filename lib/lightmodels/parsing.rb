f57d9b14319e8bdb8156cbd9fdc9430d27962692
ftomassetti <FTomassetti@FTOMASSETTI-PC>
2013-09-14 10:04:42 +0200

    Methods from class to instance

--- lib/lightmodels/parsing.rb
+++ lib/lightmodels/parsing.rb
@@ -117,7 +117,7 @@ def node_to_model(node)
 	instance
 end
 
-def self.get_feature_value_through_getter(node,feat_name)
+def get_feature_value_through_getter(node,feat_name)
 	capitalized_name = feat_name.proper_capitalize
 	methods = [:"get#{capitalized_name}",:"is#{capitalized_name}"]
 
@@ -133,7 +133,7 @@ def self.get_feature_value_through_getter(node,feat_name)
 	raise "how should I get this... #{feat_name} on #{node.class}. It does not respond to #{methods}"
 end
 
-def self.get_feature_value(node,feat_name)
+def get_feature_value(node,feat_name)
 	adapter = adapter(node.class,feat_name)		
 	if adapter
 		#puts "Using adapter for #{node.class} #{feat_name}"
