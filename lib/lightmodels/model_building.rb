302fa6d0302934494c278a982b6b86b1f1819b6a
ftomassetti <FTomassetti@FTOMASSETTI-PC>
2013-09-14 13:48:24 +0200

    Added handling models not serialized

--- lib/lightmodels/model_building.rb
+++ lib/lightmodels/model_building.rb
@@ -8,6 +8,20 @@ class << self
 	attr_accessor :verbose
 end
 
+def self.handle_serialized_models_in_dir(src,src_extension,error_handler=nil,model_handler,&model_creator)
+	puts "== #{src} ==" if LightModels::ModelBuilding.verbose
+	Dir["#{src}/*"].each do |fd|		
+		if File.directory? fd
+			basename = File.basename(fd)
+			handle_serialized_models_in_dir("#{src}/#{basename}",src_extension,error_handler,model_handler,&model_creator)
+		else
+			if File.extname(fd)==".#{src_extension}"				
+				handle_serialized_model_per_file(fd,error_handler,model_handler,&model_creator)
+			end
+		end
+	end
+end
+
 def self.handle_models_in_dir(src,src_extension,error_handler=nil,model_handler,&model_creator)
 	puts "== #{src} ==" if LightModels::ModelBuilding.verbose
 	Dir["#{src}/*"].each do |fd|		
@@ -56,6 +70,25 @@ def self.handle_model_per_file(src,error_handler=nil,model_handler,&models_gener
 		model_handler.call(src,m)
 	end
 end
+
+def self.handle_serialized_model_per_file(src,error_handler=nil,model_handler,&models_generator)
+	puts "<Model from #{src}>"
+	
+	if error_handler
+		begin
+			m = models_generator.call(src)
+			model_handler.call(src,m)
+		rescue Exception => e
+			error_handler.call(src,e)
+		rescue
+			error_handler.call(src,nil)
+		end
+	else
+		m = models_generator.call(src)
+		model_handler.call(src,m)
+	end
+end
+
 
 def self.generate_model_per_file(src,dest,max_nesting=500,error_handler=nil,&models_generator)
 	if not File.exist? dest 
