1b9d1d810d0fa2099eb9f00e287b55ca1638d163
ftomassetti <FTomassetti@FTOMASSETTI-PC>
2013-09-21 22:10:40 +0200

    Refactoring model building

--- lib/lightmodels/model_building.rb
+++ lib/lightmodels/model_building.rb
@@ -8,36 +8,26 @@ class << self
 	attr_accessor :verbose
 end
 
+def self.verbose_warn(msg)
+	warn(msg) if verbose
+end
+
 def self.handle_serialized_models_in_dir(src,src_extension,error_handler=nil,model_handler,&model_creator)
-	puts "== #{src} ==" if LightModels::ModelBuilding.verbose
-	Dir["#{src}/*"].each do |fd|		
-		if File.directory? fd
-			basename = File.basename(fd)
-			handle_serialized_models_in_dir("#{src}/#{basename}",src_extension,error_handler,model_handler,&model_creator)
-		else
-			if File.extname(fd)==".#{src_extension}"				
-				handle_serialized_model_per_file(fd,error_handler,model_handler,&model_creator)
-			end
-		end
+	Dir["**/*.#{src_extension}"].each do |fd|
+		verbose_warn "== #{fd} =="
+		handle_serialized_model_per_file(fd,error_handler,model_handler,&model_creator)		
 	end
 end
 
 def self.handle_models_in_dir(src,src_extension,error_handler=nil,model_handler,&model_creator)
-	puts "== #{src} ==" if LightModels::ModelBuilding.verbose
-	Dir["#{src}/*"].each do |fd|		
-		if File.directory? fd
-			basename = File.basename(fd)
-			handle_models_in_dir("#{src}/#{basename}",src_extension,error_handler,model_handler,&model_creator)
-		else
-			if File.extname(fd)==".#{src_extension}"				
-				handle_model_per_file(fd,error_handler,model_handler,&model_creator)
-			end
-		end
+	Dir["**/*.#{src_extension}"].each do |fd|	
+		verbose_warn "== #{fd} =="			
+		handle_model_per_file(fd,error_handler,model_handler,&model_creator)		
 	end
 end
 
 def self.generate_models_in_dir(src,dest,src_extension,dest_extension,max_nesting=500,error_handler=nil,&model_creator)
-	puts "== #{src} -> #{dest} ==" if LightModels::ModelBuilding.verbose
+	verbose_warn "== #{src} -> #{dest} =="
 	Dir["#{src}/*"].each do |fd|		
 		if File.directory? fd
 			basename = File.basename(fd)
@@ -46,7 +36,7 @@ def self.generate_models_in_dir(src,dest,src_extension,dest_extension,max_nestin
 			if File.extname(fd)==".#{src_extension}"
 				translated_simple_name = "#{File.basename(fd, ".#{src_extension}")}.#{dest_extension}"
 				translated_name = "#{dest}/#{translated_simple_name}"
-				puts "* #{fd} --> #{translated_name}" if LightModels::ModelBuilding.verbose
+				verbose_warn "* #{fd} --> #{translated_name}"
 				generate_model_per_file(fd,translated_name,max_nesting,error_handler,&model_creator)
 			end
 		end
@@ -54,7 +44,7 @@ def self.generate_models_in_dir(src,dest,src_extension,dest_extension,max_nestin
 end
 
 def self.handle_model_per_file(src,error_handler=nil,model_handler,&models_generator)
-	puts "<Model from #{src}>" if LightModels::ModelBuilding.verbose
+	verbose_warn "<Model from #{src}>"
 	
 	if error_handler
 		begin
@@ -72,7 +62,7 @@ def self.handle_model_per_file(src,error_handler=nil,model_handler,&models_gener
 end
 
 def self.handle_serialized_model_per_file(src,error_handler=nil,model_handler,&models_generator)
-	puts "<Model from #{src}>" if LightModels::ModelBuilding.verbose
+	verbose_warn "<Model from #{src}>"
 	
 	if error_handler
 		begin
@@ -92,7 +82,7 @@ end
 
 def self.generate_model_per_file(src,dest,max_nesting=500,error_handler=nil,&models_generator)
 	if not File.exist? dest 
-		puts "<Model from #{src}>" if LightModels::ModelBuilding.verbose
+		verbose_warn "<Model from #{src}>"
 		
 		if error_handler
 			begin
@@ -108,7 +98,7 @@ def self.generate_model_per_file(src,dest,max_nesting=500,error_handler=nil,&mod
 			LightModels::Serialization.save_model(m,dest,max_nesting)
 		end
 	else
-		puts "skipping #{src} because #{dest} found" if LightModels::ModelBuilding.verbose 
+		verbose_warn "skipping #{src} because #{dest} found"
 	end
 end
 
