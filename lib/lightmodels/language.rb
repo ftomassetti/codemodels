c8100dddb4056b6f19d4088cc0ebf29c534aa4ae
ftomassetti <FTomassetti@FTOMASSETTI-PC>
2013-09-23 21:44:14 +0200

    method LightModels.parse_file added

--- lib/lightmodels/language.rb
+++ lib/lightmodels/language.rb
@@ -17,6 +17,12 @@ class Language
 		parse_code(code)
 	end
 
+	def can_parse?(path)
+		extension = File.extname(path)
+		extension=extension[1..-1] if extension.length>0		
+		@extensions.include?(extension)
+	end
+
 end
 
 def self.register_language(language)
@@ -25,6 +31,12 @@ end
 
 def self.registered_languages
 	@@languages
+end
+
+def self.parse_file(path)
+	l = @@languages.find {|l| l.can_parse?(path) }
+	raise "I don't know how to pars #{path}" unless l
+	l.parse_file(path)
 end
 
 end
