56bfa2921602a874e9f609e0afc87416050e1372
ftomassetti <FTomassetti@FTOMASSETTI-PC>
2013-09-30 20:57:07 +0200

    Working on test coverage

--- lib/codemodels/language.rb
+++ lib/codemodels/language.rb
@@ -48,7 +48,9 @@ class Language
 
 end
 
+# It avoids multiple registration of the same class
 def self.register_language(language)
+	return if @@languages.find {|l| l.is_a?(language.class)}
 	@@languages << language
 end
 
@@ -56,9 +58,17 @@ def self.registered_languages
 	@@languages
 end
 
+class NoLanguageRegistered < Exception
+	attr_reader :path
+
+	def initialize(path)
+		@path = path
+	end
+end
+
 def self.parse_file(path)
 	l = @@languages.find {|l| l.can_parse?(path) }
-	raise "I don't know how to parse #{path}. Languages known: #{@@languages.map(&:name)}" unless l
+	raise NoLanguageRegistered.new(path) unless l
 	l.parser.parse_file(path)
 end
 
--- test/test_language.rb
+++ test/test_language.rb
@@ -5,16 +5,30 @@ class TestLanguage < Test::Unit::TestCase
 include CodeModels
 
 class MyLanguage < Language
-	def initialize
+	def initialize(my_parser)
 		super('MyLanguage')
 		@extensions << 'my1'
 		@extensions << 'my2'
-		@parser = 'p'
+		@parser = my_parser
+	end
+end
+
+class MyParser
+	attr_reader :invokations
+
+	def initialize
+		@invokations = []
+	end
+
+	def parse_file(path)
+		@invokations << path
 	end
 end
 
 def setup
-	CodeModels.register_language(MyLanguage.new)
+	@my_language = MyLanguage.new(MyParser.new)
+	2.times {CodeModels.register_language(@my_language)}
+	@my_parser = CodeModels.registered_languages[0].parser
 end
 
 def test_my_language
@@ -22,7 +36,23 @@ def test_my_language
 	l = CodeModels.registered_languages[0]
 	assert_equal 'MyLanguage',l.name
 	assert_equal ['my1','my2'],l.extensions
-	assert_equal 'p',l.parser
+	assert l.parser.is_a?(MyParser)
+end
+
+def test_can_parse?
+	assert_equal true, @my_language.can_parse?('pippo.my1')
+	assert_equal false, @my_language.can_parse?('pippo.else')
+end
+
+def test_parse_file_registered_language
+	CodeModels.parse_file('pippo.my1')
+	assert_equal ['pippo.my1'],@my_parser.invokations
+end
+
+def test_parse_file_unregistered_language
+	assert_raise NoLanguageRegistered do 
+		CodeModels.parse_file('pippo.else')
+	end
 end
 
 end
