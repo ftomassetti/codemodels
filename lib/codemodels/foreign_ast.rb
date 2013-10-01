4658429f0e986b9ea4676550e0a4e2ae52456d18
Federico Tomassetti <f.tomassetti@gmail.com>
2013-10-01 13:30:23 +0200

    Avoiding double inclusion of enable_foreign_asts

--- lib/codemodels/foreign_ast.rb
+++ lib/codemodels/foreign_ast.rb
@@ -3,9 +3,8 @@ require 'rgen/metamodel_builder'
 module CodeModels
 
 def self.enable_foreign_asts(clazz)
-	clazz.class_eval do
-		contains_many_uni 'foreign_asts', CodeModelsAstNode		
-	end
+	return if clazz.instance_methods.find{|m| m==:foreign_asts}
+	clazz.contains_many_uni('foreign_asts', CodeModelsAstNode)	
 end
 
 end
