20edaa735b4dc5990581eeaab0e510e7cdb80e20
ftomassetti <FTomassetti@FTOMASSETTI-PC>
2013-10-02 15:04:32 +0200

    wip

--- lib/codemodels/foreign_ast.rb
+++ lib/codemodels/foreign_ast.rb
@@ -3,7 +3,7 @@ require 'rgen/metamodel_builder'
 module CodeModels
 
 def self.enable_foreign_asts(clazz)
-	return if clazz.instance_methods.find{|m| m==:foreign_asts}
+	return if clazz.instance_methods.include?(:foreign_asts)
 	clazz.contains_many_uni('foreign_asts', CodeModelsAstNode)	
 end
 
