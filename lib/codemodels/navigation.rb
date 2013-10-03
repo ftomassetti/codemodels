f0008cf7b695e5ea12007731fdae9db413f1134b
ftomassetti <FTomassetti@FTOMASSETTI-PC>
2013-10-03 12:51:01 +0200

    wip

--- lib/codemodels/navigation.rb
+++ lib/codemodels/navigation.rb
@@ -6,7 +6,7 @@ end
 
 def self.all_children_deep_also_foreign(node)
 	arr = []
-	all_children_also_foreign(n) do |c|
+	all_children_also_foreign(node) do |c|
 		arr << c
 		arr.merge(all_children_deep_also_foreign(c))
 	end			
