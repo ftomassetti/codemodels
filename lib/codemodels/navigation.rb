23d0dc79cc4a42bde4cd9a837e9cbd9d9e304447
ftomassetti <FTomassetti@FTOMASSETTI-PC>
2013-10-03 12:53:13 +0200

    wip

--- lib/codemodels/navigation.rb
+++ lib/codemodels/navigation.rb
@@ -1,20 +1,20 @@
 module CodeModels
 
 def self.all_children_also_foreign(node)
-	node.all_children.merge(node.foreign_asts)
+	node.all_children.concat(node.foreign_asts)
 end
 
 def self.all_children_deep_also_foreign(node)
 	arr = []
 	all_children_also_foreign(node) do |c|
 		arr << c
-		arr.merge(all_children_deep_also_foreign(c))
+		arr.concat(all_children_deep_also_foreign(c))
 	end			
 	arr
 end
 
 def self.traverse_also_foreign(node,&block)
-	[node].merge(all_children_deep_also_foreign(node)).each do |n|
+	[node].concat(all_children_deep_also_foreign(node)).each do |n|
 		block.call(n)
 	end
 end
