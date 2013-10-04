519840412ece7acc9da8d7f33c8bbea91282d539
Federico Tomassetti <f.tomassetti@gmail.com>
2013-10-04 17:30:47 +0200

    Navigation classes added

--- lib/codemodels/foreign_navigation.rb
+++ lib/codemodels/foreign_navigation.rb
@@ -1,5 +1,49 @@
 module CodeModels
 
+class AbstractNavigation
+
+	def all_children_deep(node)
+		arr = []
+		all_children(node).each do |c|
+			arr << c
+			arr.concat(all_children_deep(node))
+		end			
+		arr		
+	end	
+
+	def traverse(node,&block)
+		[node].concat(all_children_deep(node)).each do |n|
+			block.call(n)
+		end
+	end	
+
+end
+
+class NavigationExcludingForeign < AbstractNavigation
+
+	def container(node)
+		node.eContainer
+	end
+
+	def all_children(node)
+		node.all_children
+	end	
+
+end
+
+class NavigationIncludingForeign < AbstractNavigation
+
+	def container(node)
+		return node.eContainer if node.eContainer
+		node.foreign_container
+	end
+
+	def all_children(node)
+		node.all_children.concat(node.foreign_asts)
+	end
+
+end
+
 def self.container_also_foreign(node)
 	return node.eContainer if node.eContainer
 	return node.foreign_container
@@ -25,11 +69,9 @@ def self.traverse_also_foreign(node,&block)
 end
 
 def self.collect_values_with_count_subtree_also_foreign(node)
-	values = node.collect_values_with_count
-	all_children_deep_also_foreign(node).each do |c|
-		c.collect_values_with_count.each do |k,v|
-			values[k]+=v
-		end
+	values = []
+	traverse_also_foreign(node) do |n|
+		n.collect_values_with_count.each { |k,v| values[k]+=v }
 	end
 	values	
 end
