5ac8f4da1424804583237bd2f09f8d719a65967a
Federico Tomassetti <f.tomassetti@gmail.com>
2014-01-12 17:29:18 +0100

    Parser documented

--- lib/codemodels/parser.rb
+++ lib/codemodels/parser.rb
@@ -4,38 +4,41 @@ require 'codemodels/monkey_patching'
 
 module CodeModels
 
+# A parser get code and produce an AST
+#
+# Sub-classes should provide internal_parse_artifact
 class Parser
 
 	DEFAULT_INTERNAL_ENCODING = 'UTF-8'
 
+	# Internal encoding. 
+	# Code is internally converted to this encoding.
 	attr_reader :internal_encoding
 
 	def initialize(internal_encoding=DEFAULT_INTERNAL_ENCODING)
 		@internal_encoding = internal_encoding
 		puts "WARN using an internal encoding different from the local encoding..." if "".encoding.name!=internal_encoding
+		raise "The method internal_parse_artifact should be implemented" unless self.respond_to?(:intern).should be_true
 	end
 
 	def parse_artifact(artifact)
 		internal_parse_artifact(artifact)
 	end
 
+	# Parse the file by producing an artifact corresponding to the file
 	def parse_file(path,file_encoding=nil)
 		file_encoding = internal_encoding unless file_encoding
 		code = IO.read(path,{ :encoding => file_encoding, :mode => 'rb'})
 		code = code.encode(internal_encoding)
 		artifact = FileArtifact.new(path,code)
-		internal_parse_artifact(artifact)
+		parse_artifact(artifact)
 	end
 
-	# Deprecated: use parse_string
-	def parse_code(code)
-		parse_string(code)
-	end
-
+	# Parse the file by producing an artifact corresponding to the string
 	def parse_string(code)
 		raise "Wrong encoding: it is #{code.encoding.name}, internally expected #{internal_encoding}" unless code.encoding.name==internal_encoding
 		artifact = StringArtifact.new(code)
-		internal_parse_artifact(artifact)
+		parse_artifact(artifact)
 	end
 
 end
@@ -43,15 +46,24 @@ end
 class ParsingError < Exception
  	attr_reader :node
  	attr_reader :line
+ 	attr_reader :column
 
- 	def initialize(node,msg,line=nil)
+ 	def initialize(node,msg,line=nil,column=nil)
  		@node = node
  		@msg = msg
  		@line = line
+ 		@column = column
+ 		raise "If column is specified also line should be" if line==nil and column!=nil
  	end
 
  	def to_s
- 		"#{@msg}, start line: #{@line}"
+ 		if @line and @column
+ 			"Parsing error: #{@msg}, node: #{node}, line: #{@line}, column: #{column}"
+ 		elsif @line
+			"Parsing error: #{@msg}, node: #{node}, line: #{@line}"
+		else
+			"Parsing error: #{@msg}, node: #{node}"
+		end
  	end
 
 end
@@ -60,10 +72,14 @@ end
 # wrapping another parser and adapting it
 # to CodeModels. When they encounter a node type
 # they do not know how to wrap this error is thrown.
+#
 # This is not just for java based parsers, so it should
 # be not moved to codemodels-javaparserwrapper
 class UnknownNodeType < ParsingError
 
+	# @param node the node that can not be translated
+	# @param node_type node that is not understood
+	# @param where describe where the node was contained (artifact, line, column)
  	def initialize(node,line=nil,node_type=nil,where=nil)
  		super(node,"UnknownNodeType: type=#{node_type} , where: #{where}")
  	end
